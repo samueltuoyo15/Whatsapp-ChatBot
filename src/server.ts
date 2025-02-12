ccee0fcd421f8535a3814414f66a83399951470a
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-02-12 23:37:01 +0100

    open ai intergration

--- src/server.ts
+++ src/server.ts
@@ -8,9 +8,35 @@ const server: Application = express()
 server.use(cors())
 server.use(bodyParser.json())
 
-const generateResponse = () => {
-  
+export const generate = async (prompt: string): Promise<string> => {
+
+  if (!prompt) {
+    return res.status(400).json({ message: "Invalid or missing prompt" })
+  }
+
+  const systemInstructions = `
+    Act like a whatsap WhatsApp ai agent. u are a ChatBot agent for Nexis Botix an ai tech powered driven startup specializing in creating chatbots, ai agents e.tc. 
+  `
+  const userPrompt = `
+    ${systemInstructions}
+
+    User: ${prompt}
+    Bot:
+  `
+
+  try {
+    const result = await textModel.generateContent(userPrompt)
+     const response: string | undefined = result.response.text()
+    return response 
+  } catch (error: any) {
+    console.error("Error generating content:", error)
+    res.status(500).json({
+      message: "Internal server error",
+      error: error instanceof Error ? error.message : "Unknown error",
+    })
+  }
 }
+
 server.get("/webhook", (req: Request, res: Response) => {
   const mode = req.query["hub.mode"] as string 
   const token = req.query["hub.verify_token"] as string 
@@ -39,7 +65,7 @@ server.post("/webhook", async (req: Request, res: Response) => {
       
       console.log(`Message from ${from}: ${text}`)
       
-      const aiResponse = await generateResponse(text)
+      const aiResponse = await generate(text)
       await sendMessage(from, aiResponse)
     }
   }
