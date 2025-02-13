5c13c0f9d67abd60e07c3fa228dcdac66f1994b0
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-02-13 09:33:17 +0100

    chatbot integration

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, {Application, Request, Response} from "express"
 import cors from "cors"
+import { Client, LocalAuth } from "whatsapp-web.js"
+import qrcode from "qrcode-terminal"
 import bodyParser from "body-parser"
 import axios from "axios"
 import { GoogleGenerativeAI } from "@google/generative-ai"
@@ -10,9 +12,27 @@ const server: Application = express()
 server.use(cors())
 server.use(bodyParser.json())
 
+const client = new Client({
+  authStrategy: new LocalAuth(),
+})
+
+client.on("qr", (qr) => {
+  console.log("Scan yhe qr code to login.")
+  qrcode.generate(qr, {small: true})
+})
+
+client.on("ready", () => {
+  console.log("What'sapp Bot is Ready")
+})
+
+client.on("message", async (message) => {
+  console.log(`Message from ${message.from}: ${message.body}`)
+  const aiResponse = await generate(message.body)
+  client.sendMessage(message.from, aiResponse)
+})
+
 const genAI = new GoogleGenerativeAI(process.env.API_KEY || "");
 const textModel = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
-
 export const generate = async (prompt: string): Promise<string> => {
 
   if (!prompt) {
@@ -32,68 +52,6 @@ export const generate = async (prompt: string): Promise<string> => {
   }
 }
 
-server.get("/webhook", (req: Request, res: Response) => {
-  const mode = req.query["hub.mode"] as string 
-  const token = req.query["hub.verify_token"] as string 
-  const challenge = req.query["hub.challenge"] as string 
-  
-  if(mode === "subscribe" && token === process.env.VERIFY_TOKEN){
-    console.log("WebHook Verified")
-    res.status(200).send(challenge)
-  }else{
-    res.sendStatus(403)
-  }
-})
 
-server.post("/webhook", async (req: Request, res: Response) => {
-  const body = req.body
-  if(!body) {
-    res.status(400).send({message: "Empty Body"})
-    return 
-  }
-  
-  try{
-    if(body.object === "whatsapp_business_account"){
-    const entry = body.entry?.[0]
-    const changes = entry.changes?.[0]
-    const message = changes?.value?.messages?.[0]
-    
-    if(message){
-      const from = message.from
-      const text = message.text?.body
-      
-      console.log(`Message from ${from}: ${text}`)
-      
-      const aiResponse = await generate(text)
-      await sendMessage(from, aiResponse)
-    }}
-    res.sendStatus(200)
-  }catch(error: any){
-    console.error(error)
-    res.status(500).send({message: "an error occurred "})
-    return 
-  }
-})
-
-const sendMessage = async (to: string, text: string) => {
-  try{
-       await axios.post(`https://graph.facebook.com/v18.0/${process.env.WHATSAPP_PHONE_ID}/messages`,
-       {
-         messaging_product: "whatsapp",
-         to,
-         text: {body: text},
-       },
-       {
-         headers: {
-           Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
-           "Content-Type": "application/json",
-         },
-       }
-       )
-     console.log(`Sent message to ${to}: ${text}`);
-  } catch(error){
-    console.log(error)
-  }
-}
 
 server.listen(Number(process.env.PORT || 3000), () => console.log("Server has started running at " + process.env.PORT))
